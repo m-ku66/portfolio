abd2e8a9ed8d7ce479544b8fed4de5213d1e9cec
m-ku66 <johanmarcmiango15@gmail.com>
2024-05-16 14:36:08 -0400

    new content and site update message

--- src/components/DesignWorks.jsx
+++ src/components/DesignWorks.jsx
@@ -1,79 +1,165 @@
 const DesignWorks = [
-    {
-        thumbnail: "/project-files/design/br-kiosk/cover.png",
-        name: "Baskin Robbins Kiosk",
-        desc: ['05/03/2023', 'Trying to design an ordering kiosk for Baskin Robbins'],
-        source: "https://player.vimeo.com/video/823520004?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        problemStatement: 'My goal for this project was to design a kiosk that could streeamline the ordering process at Baskin Robbins. Information densitiy anjd organization were key in this project, so despite all of the technical UI/UX problems a question I had to ask myself was "how do I compress this large catalogue of info into degestible and traversible content?"',
-        solution: "The first step was to do a heuristic evalutation on other existing kiosks. After looking at what worked and what didn't, I created a document that would organize all of the information I'd need for the kiosk and specific device requirements.",
-        process: ["/project-files/design/br-kiosk/process.png", "Freehand sketching some measurements for kiosk dimensions and the size relation to actual users"],
-        conclusion: "After tons of trial and error, crafting a user flow diagram, and experimenting with screen sizes the final result was a simple kiosk that mainly grouped similar items into larger, easier-to-manage categories.",
-        toolsUsed: ['Figma', 'Clip Studio Paint', 'Adobe After Effects'],
-        isEmbed: true,
-        img: 'link to img',
-        caseStudy: "/project-files/design/br-kiosk/br-process-doc.pdf",
-        files: "/project-files/design/br-kiosk/source.zip",
-    },
-    {
-        thumbnail: "/project-files/design/ddl/cover.png",
-        name: "Digital Driver's License",
-        desc: ['02/23/2023', 'A project in which I attempt to digitize a driver\'s license'],
-        source: "https://player.vimeo.com/video/801842633?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        problemStatement: "For this project I looked at physical driver's licenses and thought about their pain points. They're jam packed with information, harming their readability and physical; meaning that if you lose them, it can be a hassle to get a replacement. How can the information on the license be made to-the-point and readable?",
-        solution: "I was heavily inspired by digital finace apps like Cashapp and Venmo, and employee cards that're sometimes used in certain workplaces. Both of those things have a way of organizing information into manageable chunks that I thought would be useful for the solution to my design problem.",
-        process: ["/project-files/design/ddl/process.png", "Sketches of multiple styles/iterations of the license I had in my head"],
-        conclusion: "By prioritizing important information and then organizing it, I managed to create a proof of concept digital driver's license that tries to make using a license seamless and cool for the user.",
-        toolsUsed: ['Figma', 'Adobe After Effects'],
-        isEmbed: true,
-        img: 'link to img',
-        caseStudy: "/project-files/design/ddl/ddl-process-doc.pdf",
-        files: "/project-files/design/ddl/source.zip",
-    },
-    {
-        thumbnail: "/project-files/design/encryption/cover.png",
-        name: "Encryption",
-        desc: ['09/22/2022', 'A word animation that depicts how I see the word "Encryption"(or rather I saw it at the time of making this)'],
-        source: "https://player.vimeo.com/video/901654454?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        problemStatement: "I wanted to make a word animation based on the word 'encryption'. A lot of the problem with this is figuring out how to depict the word's meaning without being too on-the-nose.",
-        solution: "I started out by brainstorming ideas and then deciding on the vibe I wanted to go for; I had already been working on a language for some time before this project so I thought that I'd incorporate it as well.",
-        process: ["/project-files/design/encryption/process.png", "formally creating glyphs for the alphabet of my language 'Gdnsua' and making a number system for funzies"],
-        conclusion: "After everything's said and done, the final result was an animation that I think captured the cryptic feeling of the word encryption while portraying some cool techno vibes as well. This project was much more intuition-driven than my others, so there really isn't a formal case study.",
-        toolsUsed: ['Figma', 'Adobe After Effects'],
-        isEmbed: true,
-        img: 'link to img',
-        caseStudy: "https://www.figma.com/file/OL3YIBvPr4LVvIfrabPmeX/WIP_enc?type=design&mode=design&t=VyvjmLR07DVwzl9m-1",
-        files: "/project-files/design/encryption/source.zip",
-    },
-    {
-        thumbnail: "/project-files/design/mandala/cover.png",
-        name: "Mandala Project",
-        desc: ['02/17/2022', 'Using Adobe After Effects to make some cool mandala-themed motion graphics stuff'],
-        source: "https://player.vimeo.com/video/903000582?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        problemStatement: "I wanted to create a motion graphics piece that represented who I was as a person/designer in the year of 2022. How could I achieve this while sticking to a mandala motif?",
-        solution: "I took inspiration from various sources of media like manga, commercials, vector graphic art, etc. I really wanted to lead the viewer through an experience instead of showing them cool things, so I used that desire as a base for my transitions and poster shots.",
-        process: ["/project-files/design/mandala/process.png", "Some early concept sketches of the project and ideas that I wanted to incorporate"],
-        conclusion: "The end result is something that I'm proud of to this day. It isn't perfect by any means, and can definitely benefit from improved timing and some revisions, but it's a piece I'm glad I made nonetheless.",
-        toolsUsed: ['Adobe After Effects', 'Music: Chronograph from Clockwork Planet'],
-        isEmbed: true,
-        img: 'link to img',
-        caseStudy: "/project-files/design/mandala/mandala-process-doc.pdf",
-        files: "/project-files/design/mandala/source.zip",
-    },
-    {
-        thumbnail: "/project-files/design/surreal-world/cover.png",
-        name: "Surreal World",
-        desc: ['04/06/2022', 'A project in which I lead the viewer through a fantasy world.'],
-        source: "https://player.vimeo.com/video/903014835?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-        problemStatement: "I wanted to immerse the viewer into a fantasy setting that would change over time. How did I go about doing this while keeping the style and overall piece together?",
-        solution: "I decided to simplify the style and use vector graphics and basic shapes across scenes to mae sure that each one retained the same style. I als use camera movements to guide the viewer through the scenes in ways that I thought would be interesting.",
-        process: ["/project-files/design/surreal-world/process.png", "A page from my pitch deck showcasing the ideas I'd be going for."],
-        conclusion: "All of that lead to a piece that guides the viewer through a fantasy world of many scenes.",
-        toolsUsed: ['Adobe After Effects', 'Adobe Illustrator', 'Music: Hanging Garden by Sakuzyo'],
-        isEmbed: true,
-        img: 'link to img',
-        caseStudy: "/project-files/design/surreal-world/surreal-world-process-doc.pdf",
-        files: "/project-files/design/surreal-world/source.aep",
-    },
-]
+  {
+    thumbnail: "/project-files/design/web_designs/cover.png",
+    name: "Homepage Designs",
+    desc: [
+      "05/16/2024",
+      "A collection of some of the homepage desings I've created",
+    ],
+    source:
+      "https://www.figma.com/proto/WxEUdfmLn1j6sEv8ChDAgC/Web_Designs?node-id=4-2&t=hm3YnLvOsf4RArin-1&scaling=contain&page-id=0%3A1",
+    problemStatement:
+      "What kind of content I would need on my portfolio to showcase more nof my design skills? I'm aiming for a career in front-end web dev so the content should reflect skills related to that at the very least.",
+    solution:
+      "I decided to design a few mock homepages to display my current design skills and bit of my design process. Using Figma and some royalty free images, I designed some pages that would serve as a starting point for my web development projects.",
+    process: [
+      "/project-files/design/web_designs/process.png",
+      "Some of the styling work that went into these designs",
+    ],
+    conclusion:
+      "After a few hours of work I managed to make some designs that are somewhat distinct in style and mood. They serve as reference points that I recreate in code and further refine.",
+    toolsUsed: ["Figma"],
+    isEmbed: false,
+    img: "/project-files/design/web_designs/cover.png",
+    caseStudy:
+      "https://www.figma.com/design/WxEUdfmLn1j6sEv8ChDAgC/Web_Designs?node-id=0-1&t=NFGeWc7393DWImpI-0",
+    files:
+      "https://www.figma.com/design/WxEUdfmLn1j6sEv8ChDAgC/Web_Designs?node-id=0-1&t=NFGeWc7393DWImpI-0",
+  },
+  {
+    thumbnail: "/project-files/design/br-kiosk/cover.png",
+    name: "Baskin Robbins Kiosk",
+    desc: [
+      "05/03/2023",
+      "Trying to design an ordering kiosk for Baskin Robbins",
+    ],
+    source:
+      "https://player.vimeo.com/video/823520004?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    problemStatement:
+      'My goal for this project was to design a kiosk that could streeamline the ordering process at Baskin Robbins. Information densitiy anjd organization were key in this project, so despite all of the technical UI/UX problems a question I had to ask myself was "how do I compress this large catalogue of info into degestible and traversible content?"',
+    solution:
+      "The first step was to do a heuristic evalutation on other existing kiosks. After looking at what worked and what didn't, I created a document that would organize all of the information I'd need for the kiosk and specific device requirements.",
+    process: [
+      "/project-files/design/br-kiosk/process.png",
+      "Freehand sketching some measurements for kiosk dimensions and the size relation to actual users",
+    ],
+    conclusion:
+      "After tons of trial and error, crafting a user flow diagram, and experimenting with screen sizes the final result was a simple kiosk that mainly grouped similar items into larger, easier-to-manage categories.",
+    toolsUsed: ["Figma", "Clip Studio Paint", "Adobe After Effects"],
+    isEmbed: true,
+    img: "link to img",
+    caseStudy: "/project-files/design/br-kiosk/br-process-doc.pdf",
+    files: "/project-files/design/br-kiosk/source.zip",
+  },
+  {
+    thumbnail: "/project-files/design/ddl/cover.png",
+    name: "Digital Driver's License",
+    desc: [
+      "02/23/2023",
+      "A project in which I attempt to digitize a driver's license",
+    ],
+    source:
+      "https://player.vimeo.com/video/801842633?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    problemStatement:
+      "For this project I looked at physical driver's licenses and thought about their pain points. They're jam packed with information, harming their readability and physical; meaning that if you lose them, it can be a hassle to get a replacement. How can the information on the license be made to-the-point and readable?",
+    solution:
+      "I was heavily inspired by digital finace apps like Cashapp and Venmo, and employee cards that're sometimes used in certain workplaces. Both of those things have a way of organizing information into manageable chunks that I thought would be useful for the solution to my design problem.",
+    process: [
+      "/project-files/design/ddl/process.png",
+      "Sketches of multiple styles/iterations of the license I had in my head",
+    ],
+    conclusion:
+      "By prioritizing important information and then organizing it, I managed to create a proof of concept digital driver's license that tries to make using a license seamless and cool for the user.",
+    toolsUsed: ["Figma", "Adobe After Effects"],
+    isEmbed: true,
+    img: "link to img",
+    caseStudy: "/project-files/design/ddl/ddl-process-doc.pdf",
+    files: "/project-files/design/ddl/source.zip",
+  },
+  {
+    thumbnail: "/project-files/design/encryption/cover.png",
+    name: "Encryption",
+    desc: [
+      "09/22/2022",
+      'A word animation that depicts how I see the word "Encryption"(or rather I saw it at the time of making this)',
+    ],
+    source:
+      "https://player.vimeo.com/video/901654454?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    problemStatement:
+      "I wanted to make a word animation based on the word 'encryption'. A lot of the problem with this is figuring out how to depict the word's meaning without being too on-the-nose.",
+    solution:
+      "I started out by brainstorming ideas and then deciding on the vibe I wanted to go for; I had already been working on a language for some time before this project so I thought that I'd incorporate it as well.",
+    process: [
+      "/project-files/design/encryption/process.png",
+      "formally creating glyphs for the alphabet of my language 'Gdnsua' and making a number system for funzies",
+    ],
+    conclusion:
+      "After everything's said and done, the final result was an animation that I think captured the cryptic feeling of the word encryption while portraying some cool techno vibes as well. This project was much more intuition-driven than my others, so there really isn't a formal case study.",
+    toolsUsed: ["Figma", "Adobe After Effects"],
+    isEmbed: true,
+    img: "link to img",
+    caseStudy:
+      "https://www.figma.com/file/OL3YIBvPr4LVvIfrabPmeX/WIP_enc?type=design&mode=design&t=VyvjmLR07DVwzl9m-1",
+    files: "/project-files/design/encryption/source.zip",
+  },
+  {
+    thumbnail: "/project-files/design/mandala/cover.png",
+    name: "Mandala Project",
+    desc: [
+      "02/17/2022",
+      "Using Adobe After Effects to make some cool mandala-themed motion graphics stuff",
+    ],
+    source:
+      "https://player.vimeo.com/video/903000582?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    problemStatement:
+      "I wanted to create a motion graphics piece that represented who I was as a person/designer in the year of 2022. How could I achieve this while sticking to a mandala motif?",
+    solution:
+      "I took inspiration from various sources of media like manga, commercials, vector graphic art, etc. I really wanted to lead the viewer through an experience instead of showing them cool things, so I used that desire as a base for my transitions and poster shots.",
+    process: [
+      "/project-files/design/mandala/process.png",
+      "Some early concept sketches of the project and ideas that I wanted to incorporate",
+    ],
+    conclusion:
+      "The end result is something that I'm proud of to this day. It isn't perfect by any means, and can definitely benefit from improved timing and some revisions, but it's a piece I'm glad I made nonetheless.",
+    toolsUsed: [
+      "Adobe After Effects",
+      "Music: Chronograph from Clockwork Planet",
+    ],
+    isEmbed: true,
+    img: "link to img",
+    caseStudy: "/project-files/design/mandala/mandala-process-doc.pdf",
+    files: "/project-files/design/mandala/source.zip",
+  },
+  {
+    thumbnail: "/project-files/design/surreal-world/cover.png",
+    name: "Surreal World",
+    desc: [
+      "04/06/2022",
+      "A project in which I lead the viewer through a fantasy world.",
+    ],
+    source:
+      "https://player.vimeo.com/video/903014835?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    problemStatement:
+      "I wanted to immerse the viewer into a fantasy setting that would change over time. How did I go about doing this while keeping the style and overall piece together?",
+    solution:
+      "I decided to simplify the style and use vector graphics and basic shapes across scenes to mae sure that each one retained the same style. I als use camera movements to guide the viewer through the scenes in ways that I thought would be interesting.",
+    process: [
+      "/project-files/design/surreal-world/process.png",
+      "A page from my pitch deck showcasing the ideas I'd be going for.",
+    ],
+    conclusion:
+      "All of that lead to a piece that guides the viewer through a fantasy world of many scenes.",
+    toolsUsed: [
+      "Adobe After Effects",
+      "Adobe Illustrator",
+      "Music: Hanging Garden by Sakuzyo",
+    ],
+    isEmbed: true,
+    img: "link to img",
+    caseStudy:
+      "/project-files/design/surreal-world/surreal-world-process-doc.pdf",
+    files: "/project-files/design/surreal-world/source.aep",
+  },
+];
 
-export default DesignWorks
+export default DesignWorks;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import HomeGal from './HomeGal';
+import React, { useState, useEffect } from "react";
+import HomeGal from "./HomeGal";
 
 const Home = ({ setSiteState }) => {
-    const [navAnim, setNavAnim] = useState(false);
+  const [navAnim, setNavAnim] = useState(false);
 
-    const navLinkStyle = "duration-500 select-none cursor-pointer poppins phone:text-[0.6rem] medium:text-[1rem] italic text-stone-400 font-[500]";
-    const galHolderStyle = "flex phone:w-[20rem] phone:h-[20rem] medium:w-[32rem] medium:h-[32rem] mt-[0%] mb-[10%]";
-    const navHolderStyle1 = "opacity-[0%] flex w-full justify-center";
-    const navHolderStyle2 = "fade1 flex w-full justify-center";
+  const navLinkStyle =
+    "duration-500 select-none cursor-pointer poppins phone:text-[0.6rem] medium:text-[1rem] italic text-stone-400 font-[500]";
+  const galHolderStyle =
+    "flex phone:w-[20rem] phone:h-[20rem] medium:w-[32rem] medium:h-[32rem] mt-[0%] mb-[10%]";
+  const navHolderStyle1 = "opacity-[0%] flex w-full justify-center";
+  const navHolderStyle2 = "fade1 flex w-full justify-center";
 
-    useEffect(() => {
-        setTimeout(() => {
-            setNavAnim(true);
-        }, 2000);
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setNavAnim(true);
+    }, 2000);
+  }, []);
 
-    return (
-        <div className='relative flex justify-center items-center w-full h-full'>
-            <div className='flex flex-col items-center'>
-                <div data-aos='zoom-out' data-aos-duration='2000' className={galHolderStyle}>
-                    <HomeGal />
-                </div>
-
-                <div className='flex'>
-                    <h1 data-aos='fade-up' data-aos-duration='3000' className='select-none poppins phone:text-[2rem] tablet:text-[3rem] text-center phone:mb-[5%] tablet:mb-[1%]'>MARC MIANGO</h1>
-                </div>
-
-                <div className={navAnim ? navHolderStyle2 : navHolderStyle1}>
-                    <p id='illust' className={navLinkStyle} onClick={() => setSiteState("illustration")}>ILLUSTRATOR</p>
-                    <p className='select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400'>|</p>
-                    <p id='dev' className={navLinkStyle} onClick={() => setSiteState("web-dev")}>PROGRAMMER</p>
-                    <p className='select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400'>|</p>
-                    <p id='music' className={navLinkStyle} onClick={() => setSiteState("music-production")}>COMPOSER</p>
-                    <p className='select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400'>|</p>
-                    <p id='des' className={navLinkStyle} onClick={() => setSiteState("design")}>DESIGNER</p>
-                </div>
-            </div>
+  return (
+    <div className="relative flex justify-center items-center w-full h-full">
+      <p className="absolute top-[1%] select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-red-500">
+        New portfolio site will be made soon...
+      </p>
+      <div className="flex flex-col items-center">
+        <div
+          data-aos="zoom-out"
+          data-aos-duration="2000"
+          className={galHolderStyle}
+        >
+          <HomeGal />
         </div>
-    )
-}
 
-export default Home
+        <div className="flex">
+          <h1
+            data-aos="fade-up"
+            data-aos-duration="3000"
+            className="select-none poppins phone:text-[2rem] tablet:text-[3rem] text-center phone:mb-[5%] tablet:mb-[1%]"
+          >
+            MARC MIANGO
+          </h1>
+        </div>
+
+        <div className={navAnim ? navHolderStyle2 : navHolderStyle1}>
+          <p
+            id="illust"
+            className={navLinkStyle}
+            onClick={() => setSiteState("illustration")}
+          >
+            ILLUSTRATOR
+          </p>
+          <p className="select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400">
+            |
+          </p>
+          <p
+            id="dev"
+            className={navLinkStyle}
+            onClick={() => setSiteState("web-dev")}
+          >
+            PROGRAMMER
+          </p>
+          <p className="select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400">
+            |
+          </p>
+          <p
+            id="music"
+            className={navLinkStyle}
+            onClick={() => setSiteState("music-production")}
+          >
+            COMPOSER
+          </p>
+          <p className="select-none mx-[3%] phone:text-[0.5rem] medium:text-[1rem] text-stone-400">
+            |
+          </p>
+          <p
+            id="des"
+            className={navLinkStyle}
+            onClick={() => setSiteState("design")}
+          >
+            DESIGNER
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
